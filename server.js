3b71e6f57b0045d65d9b4efde73d17c741a458bb
Your Name <you@example.com>
2022-07-08 08:18:54 -0700

    updated server

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const db = require("./db/conn");
 const app = express();
 const path = require('path');
 const cors = require('cors');
-const publicPath = path.join(__dirname, "..", 'public');
+
 
 const PORT = process.env.PORT || 5555;
 
-app.use(express.static(publicPath));
+app.use(__dirname, 'build');
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors())
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join("./reactmvp/public"));
 })
 
 
